bd010135044b568ece98f6ca23ea21ff52927e63
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-01-07 20:39:24 -0300

    feat(macro): add macro from betterAuth to verify user session

---
+++ src/macros/auth.ts
@@ -0,0 +1,29 @@
+import { Elysia } from 'elysia'
+import { auth } from '../lib/auth'
+
+const betterAuth = new Elysia({ name: 'better-auth' })
+  .mount(auth.handler)
+
+  // What this macro does:
+  // - Adds an `auth` property to the context
+  // - The `auth` property has a `resolve` method that checks for a valid session
+  // - The `resolve` method takes the request headers to verify the session
+  // - If the session is valid, it returns the user and session data
+  // - If the session is invalid, it returns a 401 Unauthorized status
+
+  .macro({
+    auth: {
+      async resolve({ status, request: { headers } }) {
+        const session = await auth.api.getSession({
+          headers,
+        })
+
+        if (!session) return status(401)
+
+        return {
+          user: session.user,
+          session: session.session,
+        }
+      },
+    },
+  })
---
+++ src/macros/docs.md
@@ -0,0 +1,4 @@
+# Macros: 
+like a middleware but can add functionality to Elysia's core.
+
+We get this in https://www.better-auth.com/docs/integrations/elysia#macro
