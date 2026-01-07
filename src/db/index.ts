d37bfce91b570539cc72472e935c380c326ca9ea
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-01-07 18:12:26 -0300

    feat(drizzle): add snake_case property to DB

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,4 +3,5 @@ import * as schema from './schema'
 
 export const db = drizzle(Bun.env.DATABASE_URL!, {
   schema,
+  casing: 'snake_case', // automatically convert camelCase to snake_case in DB columns
 })
